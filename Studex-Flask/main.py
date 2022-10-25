5172f24eaa2fae75ffeceaf78351374beb4d755e
Lucas Zoser <53053622+Zosin0@users.noreply.github.com>
2022-10-25 11:10:42 -0300

    comecando a criar o banco de dados

--- Studex-Flask/main.py
+++ Studex-Flask/main.py
@@ -1,10 +1,79 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "studex"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.permanent_session_lifetime = timedelta(minutes=5)
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    def __int__(self, name=None, email=None):
+        self.name = name
+        self.email = email
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
+        flash("Login com sucesso!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Ja esta logado!")
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"Voce foi deslogado, {user}!", "info")
+    session.pop("user", None)
+    session.pop("email", None)
+    return redirect(url_for("login"))
+
+@app.route("/user", methods=["POST", "GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email salvo")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
+    else:
+        flash("Voce nao esta logado!")
+        return redirect(url_for("login"))
+
 
 @app.route("/form", methods=["POST", "GET"])
 def form():
@@ -21,14 +90,19 @@ def form():
         html = request.form["html"]
         java = request.form["jv"]
         resumo = request.form["rs"]
-        return redirect(url_for("user", usr=[user, ra, semestre, tempo, linguagem, so, python, javascript, c, html, java, resumo]))
+        return redirect(url_for("pokemon", usr=[user, ra, semestre, tempo, linguagem, so, python, javascript, c, html, java, resumo]))
     else:
         return render_template("form.html", content="forms")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
 
+""" 
+@app.route("/<usr>")
+def pokemon(usr):
+    return f"<h1>{usr}</h1>"
+"""
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
