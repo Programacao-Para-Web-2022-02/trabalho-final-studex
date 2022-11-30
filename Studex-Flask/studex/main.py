2da0f6b2cdc02be0a3a39a18ba8446af49cfc5a8
Lucas Zoser <53053622+Zosin0@users.noreply.github.com>
2022-11-30 13:01:43 -0300

    Style Editar

--- Studex-Flask/studex/main.py
+++ Studex-Flask/studex/main.py
@@ -65,8 +65,8 @@ def ajaxlivesearch():
     if request.method == 'POST':
         search_word = request.form['query']
         print(search_word)
-        if search_word == '':
-            query = "SELECT * from usuario ORDER BY id"
+        if search_word == current_user.usuario:
+            query = f"SELECT usuario from usuario ORDER BY id"
             cur.execute(query)
             usuario = cur.fetchall()
         else:
@@ -79,32 +79,8 @@ def ajaxlivesearch():
 
 @app.route("/mapa")
 def mapview():
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                'lat': 37.4419,
-                'lng': -122.1419,
-                'infobox': "<b>Hello World</b>"
-            },
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                'lat': 37.4300,
-                'lng': -122.1400,
-                'infobox': "<b>Hello World from other place</b>"
-            }
-        ]
-    )
-    return render_template('mapa.html', mymap=mymap, sndmap=sndmap, user=current_user)
+
+    return render_template('mapa.html', user=current_user)
 
 
 @app.route("/login", methods=["POST", "GET"])
